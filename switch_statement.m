e75871a72059c74173066b9078c96443daf89007
sobhy mohamed mohamed elsayed <30624911+elbhwashy@users.noreply.github.com>
2017-10-14 14:55:18 +0200

    Add files via upload

---
+++ switch_statement.m
@@ -0,0 +1,16 @@
+function switch_statement()
+switch num 
+    case {-2, -1}
+        f3(num)
+    case {0, 1, 2}
+        f2(num) 
+    case {3, 4} 
+        f4(num)
+    otherwise
+        f1(num)
+end
+
+
+
+end
+
